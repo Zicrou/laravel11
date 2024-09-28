c00f1aeab4430648c941603523122c3f27f910ef
Zicrou <aazseck@gmail.com>
2024-09-28 22:33:07 +0000

    Added Post to the database, Factory

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::view('/', 'posts.index')->name('home');
+Route::redirect('/', 'posts');
 
 Route::resource('posts', PostController::class);
 
