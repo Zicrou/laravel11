16c9a19c6570be86be6e303307fa8d84f4ec2ed3
Zicrou <aazseck@gmail.com>
2024-10-05 02:22:20 +0000

    Sendin Email

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
+use App\Mail\WelcomeMail;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
@@ -28,6 +30,7 @@ class PostController extends Controller
      */
     public function index()
     {
+        //Mail::to('mike@email.com')->send(new WelcomeMail(Auth::user()));
         $posts = Post::latest()->paginate(6);
         
 
@@ -64,11 +67,15 @@ class PostController extends Controller
         
         // Create a post
         //Post::create(['user_id' => Auth::id(), ...$fields]);
-        Auth::user()->posts()->create([
+        $post = Auth::user()->posts()->create([
             'title' => $request->title,
             'body' => $request->body,
             'image' => $path,
         ]);
+
+        // Send Email
+            Mail::to(Auth::user())->send(new WelcomeMail(Auth::user(), $post));
+    
         
         // Redirect to dashboard
         return back()->with('success', 'Your post was created');
